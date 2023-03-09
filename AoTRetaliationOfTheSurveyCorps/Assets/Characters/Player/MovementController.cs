8cef8529f6e3ca7ebb9d93fc0dffed977628e325
Michael Bui <michaelbui99@hotmail.com>
2023-03-09 17:51:14 +0100

    Implemented jumping

--- AoTRetaliationOfTheSurveyCorps/Assets/Characters/Player/MovementController.cs
+++ AoTRetaliationOfTheSurveyCorps/Assets/Characters/Player/MovementController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,23 +16,41 @@ public class MovementController : MonoBehaviour
     private PlayerInput playerInput;
 
     private Rigidbody _rigidbody;
+    private Vector3 _horizontalMovementVector;
+    private bool _isGrounded = false;
 
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
     }
-    
+
+    void Start()
+    {
+    }
+
+    public void OnJump(InputAction.CallbackContext value)
+    {
+        if (_isGrounded)
+        {
+            _rigidbody.AddForce(Vector3.up * verticalSpeed);
+        }
+    }
 
     public void OnMovement(InputAction.CallbackContext value)
     {
         Vector2 inputMovement = value.ReadValue<Vector2>();
-        _rigidbody.AddForce(new Vector3(inputMovement.x, 0, inputMovement.y) * (horizontalSpeed * Time.deltaTime));
+        _horizontalMovementVector = new Vector3(inputMovement.x, 0, inputMovement.y) * horizontalSpeed;
     }
 
-    
+    private void FixedUpdate()
+    {
+        _rigidbody.AddRelativeForce(_horizontalMovementVector);
+    }
+
     // Update is called once per frame
     void Update()
     {
+        _isGrounded = _rigidbody.velocity.y == 0;
     }
 }
