78f31eb59f2d79029ef34571261626a73ccad7bb
Michael Bui <michaelbui99@hotmail.com>
2023-03-10 07:20:54 +0100

    Add event-based grounded detection for MovementController

--- AoTRetaliationOfTheSurveyCorps/Assets/Characters/Player/FollowObject.cs
+++ AoTRetaliationOfTheSurveyCorps/Assets/Characters/Player/FollowObject.cs
@@ -6,7 +6,10 @@ public class FollowObject : MonoBehaviour
 {
     [SerializeField]
     private GameObject target;
+    
+    // Current offset for player is Position(0, -1.5, 3). Rotation on camera is (-1.5, 0, 0)
     private Vector3 _offset;
+    
     // Start is called before the first frame update
     void Start()
     {
