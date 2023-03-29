9dcce60d9a4d271f181c44dd58a949f751aeea89
Michael Bui <michaelbui99@hotmail.com>
2023-03-29 20:06:03 +0200

    Refactor OnLook to seperate controller, fix bug where player could walk while not grounded and add drag

--- AoTRetaliationOfTheSurveyCorps/Assets/Camera/FollowTarget.cs
+++ AoTRetaliationOfTheSurveyCorps/Assets/Camera/FollowTarget.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class FollowTarget: MonoBehaviour
 {
     [Header("References")]
     [SerializeField]
