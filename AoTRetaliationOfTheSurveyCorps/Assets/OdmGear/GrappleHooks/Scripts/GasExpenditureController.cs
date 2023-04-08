a8c4a5d7970ce5f4a7414c5e3292bb75c09c16d7
Michael Bui <michaelbui99@hotmail.com>
2023-04-08 22:50:14 +0200

    Implemented Smart Aim

--- AoTRetaliationOfTheSurveyCorps/Assets/OdmGear/GrappleHooks/Scripts/GasExpenditureController.cs
+++ AoTRetaliationOfTheSurveyCorps/Assets/OdmGear/GrappleHooks/Scripts/GasExpenditureController.cs
@@ -17,7 +17,7 @@ namespace OdmGear.GrappleHooks.Scripts
 
         private void Awake()
         {
-            _hitDetection = gameObject.GetComponent<IHitDetection>();
+            _hitDetection = GetComponent<IHitDetection>();
         }
 
         private void Start()
@@ -32,7 +32,7 @@ namespace OdmGear.GrappleHooks.Scripts
             _hitDetection.OnHitEvent -= SpendOnHookPropulsionCost;
         }
 
-        public void SpendHookLaunchCost(Vector3 direction, float distance)
+        private void SpendHookLaunchCost(Vector3 direction, float distance)
         {
             if (!gasController.HasEnoughGas(globalHookSettings.LaunchHookGasCost))
             {
@@ -42,14 +42,14 @@ namespace OdmGear.GrappleHooks.Scripts
             gasController.SpendGas(globalHookSettings.LaunchHookGasCost);
         }
 
-        public void SpendOnHookPropulsionCost(RaycastHit hit)
+        private void SpendOnHookPropulsionCost(RaycastHit hit)
         {
-            if (!gasController.HasEnoughGas(globalHookSettings.OnHookPropulsionCost))
+            if (!gasController.HasEnoughGas(globalHookSettings.OnHookPropulsionGasCost))
             {
                 return;
             }
 
-            gasController.SpendGas(globalHookSettings.OnHookPropulsionCost);
+            gasController.SpendGas(globalHookSettings.OnHookPropulsionGasCost);
         }
     }
 }
