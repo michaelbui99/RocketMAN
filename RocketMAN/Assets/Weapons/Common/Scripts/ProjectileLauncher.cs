bdd4900f1a94f834c77228c08c085f47a9e90d29
Michael Bui <michaelbui99@hotmail.com>
2023-04-18 19:54:36 +0200

    Fixed bullet trajectory and added rocket launcher model

--- RocketMAN/Assets/Weapons/Common/Scripts/ProjectileLauncher.cs
+++ RocketMAN/Assets/Weapons/Common/Scripts/ProjectileLauncher.cs
@@ -23,12 +23,13 @@ namespace Weapons.Common.Scripts
         public void Launch()
         {
             Ray ray = RayCastUtil.GetRayToCenterOfScreen(_mainCamera);
-            
+            var target = Physics.Raycast(ray, out RaycastHit hit) ? hit.point : ray.GetPoint(1000);
+
             var projectileObject = Instantiate(projectilePrefab);
             projectileObject.transform.position = launchPoint.transform.position;
             projectileObject.transform.forward = _mainCamera.transform.forward;
             var projectile = projectileObject.GetComponent<IProjectile>();
-            projectile.Activate(ray.direction);
+            projectile.Activate(target);
         }
     }
 }
--- RocketMAN/Assets/Weapons/RocketLauncher/Scripts/Rocket.cs
+++ RocketMAN/Assets/Weapons/RocketLauncher/Scripts/Rocket.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Linq;
+using Cinemachine.Utility;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using Weapons.Common.Scripts;
 
@@ -17,11 +19,17 @@ namespace Weapons.RocketLauncher.Scripts
 
         private Rigidbody _rigidbody;
         private bool _active = false;
-        private Vector3 _direction;
+        private Vector3 _destination;
+        private Vector3 _initialPosition;
+        private float _distanceToTravel;
+        private LineRenderer _lr;
 
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody>();
+            _lr = gameObject.AddComponent<LineRenderer>();
+            _lr.startWidth = 0.1f;
+            _lr.endWidth = 0.1f;
         }
 
         private void FixedUpdate()
@@ -31,7 +39,15 @@ namespace Weapons.RocketLauncher.Scripts
                 return;
             }
 
-            _rigidbody.AddForce(_direction * (Speed * Time.fixedDeltaTime), ForceMode.Acceleration);
+            if (GetDistanceTraveled() == GetDistanceToTravel())
+            {
+                Explode();
+                Destroy(gameObject);
+            }
+
+            _lr.SetPosition(0, transform.position);
+            _lr.SetPosition(1, _destination);
+            MoveRocketTowardsDestination();
         }
 
         private void OnCollisionEnter(Collision collision)
@@ -41,26 +57,70 @@ namespace Weapons.RocketLauncher.Scripts
 
         public void OnCollision(Collision collision)
         {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                // NOTE: (mibui 2023-04-18) The player shouldn't be able to rocket jump off it's own body
+                return;
+            }
+            Explode();
+            Destroy(gameObject);
+        }
+
+        private void Explode()
+        {
             // NOTE: (mibui 2023-04-17): Reference from Rigidbody.AddExplosionForce API documentation
             Vector3 explosionPosition = transform.position;
             Collider[] colliders = Physics.OverlapSphere(explosionPosition, ExplosionRadius);
-            
+
             colliders.ToList().ForEach(hit =>
             {
                 var rb = hit.GetComponent<Rigidbody>();
                 if (rb != null)
                 {
-                    rb.AddExplosionForce(ExplosionForce, explosionPosition, ExplosionRadius, 3.0f, ForceMode.VelocityChange);
+                    rb.AddExplosionForce(ExplosionForce, explosionPosition, ExplosionRadius, 3.0f,
+                        ForceMode.VelocityChange);
                 }
             });
-            
-            Destroy(gameObject);
         }
 
-        public void Activate(Vector3 direction)
+        private float GetDistanceToTravel()
         {
-            _direction = direction;
+            return Vector3.Distance(_destination, _initialPosition);
+        }
+
+        private float GetDistanceTraveled()
+        {
+            return Vector3.Distance(transform.position, _initialPosition);
+        }
+
+        private void MoveRocketTowardsDestination()
+        {
+            var direction = (_destination - transform.position).normalized;
+            gameObject.transform.forward = direction;
+
+            _rigidbody.AddForce(direction * (Speed * Time.fixedDeltaTime), ForceMode.Acceleration);
+        }
+
+        private void MoveRocketTowardsDestination(float forceScalar)
+        {
+            var direction = (_destination - transform.position).normalized;
+            gameObject.transform.forward = direction;
+
+            _rigidbody.AddForce(direction * (Speed * forceScalar * Time.fixedDeltaTime), ForceMode.Acceleration);
+        }
+
+        private void InitialRocketPropulsion()
+        {
+            MoveRocketTowardsDestination(20);
+        }
+
+        public void Activate(Vector3 destination)
+        {
+            var pos = transform.position;
+            _initialPosition = pos;
+            _destination = destination;
             _active = true;
+            InitialRocketPropulsion();
         }
     }
 }
